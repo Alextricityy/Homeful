16525ccdbc74b6e51b62a267f2b2c733612e79a1
Alex Sell <alextricityy@gmail.com>
2018-05-29 12:44:43 +0100

    fixed

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -2,10 +2,10 @@ class Recipient < ApplicationRecord
   belongs_to :users
   has_many :items
   has_many :locations
-  mount_uploader :photo, PhotoUploader
+  # mount_uploader :photo, PhotoUploader
 
   validates :first_name, presence:true
-  validatable :gender, presence:true , inclusion: { in: %w(Male Female Robot)}
+  validates :gender, presence:true , inclusion: { in: %w(Male Female Robot)}
   validates :dob, presence:true
   validates :bio, presence:true
   validates :photo, presence:true
