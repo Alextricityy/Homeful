1fe1d87df3edd5274f58be4534d4cd32d1f106bc
Alex Sell <alextricityy@gmail.com>
2018-05-29 16:29:08 +0100

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :recipients do
     resources :locations
-    resources :items, except: [:new]
+    resources :items
   end
 end
