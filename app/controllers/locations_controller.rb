4a40f01f919d0ce25b1ea76ed2a192e4a21f961e
Hedi van Meurs <alisoun@gmail.com>
2018-05-29 14:18:53 +0100

    locations controller and routes

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,34 @@
 class LocationsController < ApplicationController
+  before_action :set_recipient, only: :create
+
+  def create
+    @location = Location.new(location_params)
+    @location.recipient = @recipient
+    if @location.safe
+      redirect_to recipient_path(@recipient)
+    else
+      render "recipient/show"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @location = location.find(params[:location_id])
+    @location.address = params[:address]
+    @location.save
+  end
+
+
+  private
+
+  def set_recipient
+    @recipient = recipient.find(params[:id])
+  end
+
+  def location_params
+    params.require(:address)
+  end
+
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,4 @@
 class Location < ApplicationRecord
   belongs_to :recipients
+  validates :address, presence:true
 end
