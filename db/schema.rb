7f01abbe14a79db5cca1ea9557de417c1c68665c
Melvin Stoltze <stoltze.melvin@gmail.com>
2018-05-31 11:37:16 +0100

    fixed schema issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_05_30_141548) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,8 +72,7 @@ ActiveRecord::Schema.define(version: 2018_05_30_141548) do
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id_id"
-    t.index ["user_id_id"], name: "index_recipients_on_user_id_id"
+    t.index ["user_id"], name: "index_recipients_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
