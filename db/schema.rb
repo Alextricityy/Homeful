375970caa68bcd2ea02e0e56c43e0d3c13e0a287
Tim Brackfield <Cloud_193@hotmail.com>
2018-05-31 17:56:06 +0100

    updated slightly index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_05_31_135947) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,7 +63,6 @@ ActiveRecord::Schema.define(version: 2018_05_31_135947) do
   end
 
   create_table "recipients", force: :cascade do |t|
-    t.bigint "user_id"
     t.string "first_name"
     t.string "last_name"
     t.string "gender"
@@ -75,6 +72,7 @@ ActiveRecord::Schema.define(version: 2018_05_31_135947) do
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_recipients_on_user_id"
   end
 
@@ -105,5 +103,4 @@ ActiveRecord::Schema.define(version: 2018_05_31_135947) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "recipients", "users"
 end
