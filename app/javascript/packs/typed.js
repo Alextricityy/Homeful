b91c86f65d3ab357569fb8c2eebaffb63ad9fd39
Alex Sell <alextricityy@gmail.com>
2018-06-01 16:24:09 +0100

    just try

--- app/javascript/packs/typed.js
+++ app/javascript/packs/typed.js
@@ -1,13 +1,21 @@
-// import Typed from 'typed.js';
+import Typed from 'typed.js';
 
-// function loadDynamicBannerText() {
-//   new Typed('#homeful-typed', {
-//     strings: ["Make meaningful donations", "Help others in need", "Stop food waste", "Volunteer to help"],
-//     typeSpeed: 80,
-//     loop: true
-//   });
-// }
+function loadDynamicBannerText() {
+  new Typed('#homeful-typed', {
+    strings: [
+    "meaningful",
+    "life changing",
+    "helpful",
+    "amazing",
+    "easy",
+    "wonderful",
+    "incredible",
+    ],
+    typeSpeed: 80,
+    loop: true
+  });
+}
 
-// if (window.location.href === "http://localhost:3000/") {
-//   loadDynamicBannerText();
-// }
+if (window.location.href === "http://www.homeful.co.uk/") {
+  loadDynamicBannerText();
+}
