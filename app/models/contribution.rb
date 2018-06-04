70df0f7427c06bfe4f82f955e257ee24447470e2
Tim Brackfield <Cloud_193@hotmail.com>
2018-06-04 12:33:06 +0100

    prevented donations of zero

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -2,6 +2,6 @@ class Contribution < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one :payment
-  validates :amount_cents, presence: true
+  validates :amount_cents, presence: true, numericality: { greater_than: 0}
   monetize :amount_cents
 end
